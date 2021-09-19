00a6f41ad16a799af4d35d8ad55f9b71e3b96049
lablet-eu <85132881+lablet-eu@users.noreply.github.com>
2021-09-19 15:52:36 +0200

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 class FileData{
     name:string
     offset:number
-    data:NumberFormat.UInt8LE[]
+    data:string[]
     constructor(name:string,offset:number){
         this.name=name
         this.offset=offset
@@ -10,12 +10,59 @@ class FileData{
 }
 
 namespace labletFS{
-    export function openFS(){
-        
+    let isOpen:boolean=false
+    let tx = SerialPin.P15
+    let rx = SerialPin.P14
+    let baud = 115200
+    let timeout = 1000
+    export function openConfiguredFS(){
+        serial.redirect(tx,rx,baud)
+        isOpen=true
+    }
+    export function readCommand(cmd:string):string{
+        serial.writeLine(cmd)
+        let t0 = control.millis()
+        let t = t0
+        let result = ""
+        while (t - t0 < timeout) {
+            let s = serial.readString()
+            if (s.length <= 0) break
+            result+=s
+            t0 = t
+            t = control.millis()
+        }
+        return result
+    }
+    export function list():String[]{
+        let r=readCommand("ls")
+        return r.split('\r\n')
     }
     export function readFile(name:string,offset:number):FileData{
         let fd=new FileData(name,offset)
-
+        serial.writeLine("read "+name+" "+offset)
+        let t0=control.millis()
+        let t=t0
+        while(t-t0<timeout){
+            let s=serial.readString()
+            if(s.length<=0)break
+            fd.data.push(s)
+            t0=t
+            t=control.millis()
+        }
+        return fd
+    }
+    export function writeFile(name: string, offset: number): FileData {
+        let fd = new FileData(name, offset)
+        serial.writeLine("write " + name + " " + offset)
+        let t0 = control.millis()
+        let t = t0
+        while (t - t0 < timeout) {
+            let s = serial.readString()
+            if (s.length <= 0) break
+            fd.data.push(s)
+            t0 = t
+            t = control.millis()
+        }
         return fd
     }
 }
